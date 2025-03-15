b36c70e62845658f9fd2d3067bc3a6e1b6b1b6e1
Diego Asterio <diegoasterio.z@gmail.com>
2025-03-15 16:56:44 +0100

    Solve day 2 part 2

--- src/exercises/day_two.rs
+++ src/exercises/day_two.rs
@@ -13,7 +13,12 @@ pub fn run (input: &PuzzleInput) -> Result<(), &'static str> {
     let reports = parse_input(&input.text)?;
 
     if input.iteration == 0 {
-        solve_fst_puzzle(reports)
+        let fault_tolerance = 0;
+        solve_puzzle(reports, fault_tolerance)
+    }
+    else if input.iteration == 1 {
+        let fault_tolerance = 1;
+        solve_puzzle(reports, fault_tolerance)
     }
     else {
         Err("Unable to solve puzzle for day 2")
@@ -37,271 +42,128 @@ fn parse_input(input: &String) -> Result<Vec<Vec<i32>>, &'static str> {
     Ok(reports)
 }
 
-fn solve_fst_puzzle(reports: Vec<Vec<i32>>) -> Result<(), &'static str>{
-    let num_safe_reports = count_safe_reports(reports);
+fn solve_puzzle(reports: Vec<Vec<i32>>, fault_tolerance: i32) -> Result<(), &'static str>{
+    let num_safe_reports = count_safe_reports(reports, fault_tolerance);
 
     println!("{num_safe_reports}");
 
     Ok(())
 }
 
-fn count_safe_reports(reports: Vec<Vec<i32>>) -> usize {
-    reports.iter().map(asses_level).filter(|x| {*x == SafetyLevel::Safe}).count()
+fn count_safe_reports(reports: Vec<Vec<i32>>, tolerance: i32) -> usize {
+    reports.iter().map(|x| {assess_level(x, tolerance)}).filter(|y| {*y == SafetyLevel::Safe}).count()
 }
 
-fn asses_level(report: &Vec<i32>) -> SafetyLevel {
-    if is_monotone(report) && adjacent_levels_dont_change_a_lot(report){
+fn assess_level(report: &Vec<i32>, tolerance: i32) -> SafetyLevel {
+    if is_safe(report, tolerance){
         SafetyLevel::Safe
     } else {
         SafetyLevel::Unsafe
     }
 }
 
-fn is_monotone(sequence: &Vec<i32>) -> bool {
-    return only_increases(sequence) || only_decreases(sequence)
+fn is_safe(report: &Vec<i32>, tolerance: i32) -> bool{
+    is_increasing_and_safe(report, tolerance) || is_increasing_and_safe(&report.iter().copied().rev().collect(), tolerance)
 }
 
-fn only_increases(xs: &Vec<i32>) -> bool {
-    let mut increases: bool = true;
-    if xs.len() >= 2 {
-        for (x0, x1) in zip(xs.iter(), xs[1..].iter()){
-            increases &= x0 <= x1
+fn is_increasing_and_safe(report: &Vec<i32>, tolerance: i32) -> bool{
+    if tolerance < 0 {
+        return false;
+    }
+    for i in 0..report.len()-1 {
+        let current = report[i];
+        let next = report[i+1];
+        let diff = next - current;
+
+        if current >= next || diff > 3{
+            let new_tolerance = tolerance - 1;
+            let mut report_without_current = report.clone();
+            let mut report_without_next =  report.clone();
+            report_without_current.remove(i);
+            report_without_next.remove(i+1);
+            return is_increasing_and_safe(&report_without_current, new_tolerance) || is_increasing_and_safe(&report_without_next, new_tolerance)
         }
     }
-    increases
-}
-
-fn only_decreases(xs: &Vec<i32>) -> bool {
-    let mut decreases: bool = true;
-    if xs.len() >= 2 {
-        for (x0, x1) in zip(xs.iter(), xs[1..].iter()){
-            decreases &= x0 >= x1
-        }
-    }
-    decreases
-}
-
-fn does_not_differ_a_lot(x: &i32, y: &i32) -> bool{
-    let d = x.abs_diff(*y);
-    1 <= d && d <= 3
-}
-
-fn adjacent_levels_dont_change_a_lot(xs: &Vec<i32>) -> bool {
-    let mut meets_conditions: bool = true;
-    if xs.len() >= 2 {
-        for (x0, x1) in zip(xs.iter(), xs[1..].iter()){
-            meets_conditions &= does_not_differ_a_lot(x0, x1)
-        }
-    }
-    meets_conditions
+    true
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::exercises::day_two::asses_level;
+    use crate::exercises::day_two::assess_level;
 
-    use super::{adjacent_levels_dont_change_a_lot, count_safe_reports, does_not_differ_a_lot, only_decreases, only_increases, SafetyLevel};
-
-    #[test]
-    fn test_only_increases_over_increasing_sequence(){
-        let should_be = true;
-
-        let xs = vec![1, 2, 3];
-
-        let only_increases_p =  only_increases(&xs);
-
-        assert_eq!(should_be, only_increases_p)
-    }
+    use super::{count_safe_reports, SafetyLevel};
 
     #[test]
-    fn test_only_increases_over_constant_sequence(){
-        let should_be = true;
+    fn report_one_puzzle_one(){
+        let tolerance = 0;
 
-        let xs = vec![1, 1, 1];
-
-        let only_increases_p = only_increases(&xs);
-
-        assert_eq!(should_be, only_increases_p);
-
-    }
-
-    #[test]
-    fn test_only_increases_over_decreasing_sequence(){
-        let should_be = false;
-
-        let xs = vec![3, 2, 1];
-
-        let only_increases_p = only_increases(&xs);
-
-        assert_eq!(should_be, only_increases_p);
-    }
-
-    #[test]
-    fn test_only_increases_over_trivial_sequence(){
-        let should_be = true;
-
-        let xs = vec![1];
-
-        let only_increases_p = only_increases(&xs);
-
-        assert_eq!(should_be, only_increases_p)
-    }
-
-    #[test]
-    fn test_only_increases_over_empty_sequence(){
-        let should_be = true;
-
-        let xs = vec![];
-
-        let only_increases_p = only_increases(&xs);
-
-        assert_eq!(should_be, only_increases_p)
-    }
-
-    #[test]
-    fn test_only_decreases_over_increasing_sequence(){
-        let should_be = false;
-
-        let xs = vec![1, 2, 3];
-
-        let only_decreases_p = only_decreases(&xs);
-
-        assert_eq!(should_be, only_decreases_p);
-    }
-
-    #[test]
-    fn test_only_decreases_over_constant_sequence(){
-        let should_be = true;
-
-        let xs = vec![1, 1, 1];
-
-        let only_decreases_p = only_decreases(&xs);
-
-        assert_eq!(should_be, only_decreases_p);
-    }
-
-    #[test]
-    fn test_only_decreases_over_decreasing_sequence(){
-        let should_be = true;
-
-        let xs = vec![3, 2, 1];
-
-        let only_decreases_p = only_decreases(&xs);
-
-        assert_eq!(should_be, only_decreases_p);
-    }
-
-    #[test]
-    fn test_only_decreases_over_trivial_sequence(){
-        let should_be = true;
-
-        let xs = vec![1];
-
-        let only_decreases_p = only_decreases(&xs);
-
-        assert_eq!(should_be, only_decreases_p);
-    }
-
-    #[test]
-    fn test_only_decreases_over_empty_sequence(){
-        let should_be = true;
-
-        let xs = vec![];
-
-        let only_decreases_p = only_decreases(&xs);
-
-        assert_eq!(should_be, only_decreases_p);
-    }
-
-    #[test]
-    fn test_does_not_differ() {
-        let should_be = false;
-
-        let result_p = does_not_differ_a_lot(&1, &1);
-
-        assert_eq!(should_be, result_p);
-    }
-
-    #[test]
-    fn test_does_not_differ_a_lot_one() {
-        let should_be = true;
-
-        let result_p = does_not_differ_a_lot(&1, &2);
-
-        assert_eq!(should_be, result_p);
-    }
-    #[test]
-    fn test_does_not_differ_a_lot_two() {
-        let should_be = true;
-
-        let result_p = does_not_differ_a_lot(&1, &3);
-
-        assert_eq!(should_be, result_p);
-    }
-    #[test]
-    fn test_does_not_differ_a_lot_three() {
-        let should_be = true;
-
-        let result_p = does_not_differ_a_lot(&1, &4);
-
-        assert_eq!(should_be, result_p);
-    }
-
-    #[test]
-    fn test_does_differ_a_lot() {
-        let should_be = false;
-
-        let result_p = does_not_differ_a_lot(&1, &5);
-
-        assert_eq!(should_be, result_p);
-    }
-
-    #[test]
-    fn test_levels_dont_change_a_lot() {
-        let should_be = true;
-
-        let xs = vec![1, 3, 6, 7, 9];
-
-        let result_p = adjacent_levels_dont_change_a_lot(&xs);
-
-        assert_eq!(should_be, result_p);
-    }
-
-    #[test]
-    fn asses_first_level() {
+        let report = vec![7, 6, 4, 2, 1];
         let should_be = SafetyLevel::Safe;
 
-        let xs = vec![7, 6, 4, 2, 1];
+        let report_safety = assess_level(&report, tolerance);
 
-        let assessment = asses_level(&xs);
-
-        assert_eq!(should_be, assessment)
+        assert_eq!(should_be, report_safety);
     }
+ #[test]
+    fn report_two_puzzle_one(){
+        let tolerance = 0;
 
-    #[test]
-    fn asses_second_level() {
+        let report = vec![1, 2, 7, 8, 9];
         let should_be = SafetyLevel::Unsafe;
 
-        let xs = vec![1, 2, 7, 8, 9];
+        let report_safety = assess_level(&report, tolerance);
 
-        let assessment = asses_level(&xs);
+        assert_eq!(should_be, report_safety);
+    }
+ #[test]
+    fn report_three_puzzle_one(){
+        let tolerance = 0;
 
-        assert_eq!(should_be, assessment)
+        let report = vec![9, 7, 6, 2, 1];
+        let should_be = SafetyLevel::Unsafe;
+
+        let report_safety = assess_level(&report, tolerance);
+
+        assert_eq!(should_be, report_safety);
+    }
+ #[test]
+    fn report_four_puzzle_one(){
+        let tolerance = 0;
+
+        let report = vec![1, 3, 2, 4, 5];
+        let should_be = SafetyLevel::Unsafe;
+
+        let report_safety = assess_level(&report, tolerance);
+
+        assert_eq!(should_be, report_safety);
     }
 
-    #[test]
-    fn asses_last_level() {
+ #[test]
+    fn report_five_puzzle_one(){
+        let tolerance = 0;
+
+        let report = vec![8, 6, 4, 4, 1];
+        let should_be = SafetyLevel::Unsafe;
+
+        let report_safety = assess_level(&report, tolerance);
+
+        assert_eq!(should_be, report_safety);
+    }
+
+ #[test]
+    fn report_six_puzzle_one(){
+        let tolerance = 0;
+
+        let report = vec![1, 3, 6, 7, 9];
         let should_be = SafetyLevel::Safe;
 
-        let xs = vec![1, 3, 6, 7, 9];
+        let report_safety = assess_level(&report, tolerance);
 
-        let assessment = asses_level(&xs);
-
-        assert_eq!(should_be, assessment)
+        assert_eq!(should_be, report_safety);
     }
 
-    #[test]
-    fn count_safe_reports_from_the_example(){
+#[test]
+    fn count_safe_reports_from_puzzle_one_example(){
         let should_be = 2;
 
         let reports = vec![
@@ -313,7 +175,83 @@ mod tests {
                         vec![1, 3, 6, 7, 9]
                      ];
 
-        let num_safe_reports = count_safe_reports(reports);
+        let num_safe_reports = count_safe_reports(reports, 0);
+
+        assert_eq!(should_be, num_safe_reports)
+    }
+
+#[test]
+    fn report_two_puzzle_two(){
+        let tolerance = 1;
+
+        let report = vec![1, 2, 7, 8, 9];
+        let should_be = SafetyLevel::Unsafe;
+
+        let report_safety = assess_level(&report, tolerance);
+
+        assert_eq!(should_be, report_safety);
+    }
+ #[test]
+    fn report_three_puzzle_two(){
+        let tolerance = 1;
+
+        let report = vec![9, 7, 6, 2, 1];
+        let should_be = SafetyLevel::Unsafe;
+
+        let report_safety = assess_level(&report, tolerance);
+
+        assert_eq!(should_be, report_safety);
+    }
+ #[test]
+    fn report_four_puzzle_two(){
+        let tolerance = 1;
+
+        let report = vec![1, 3, 2, 4, 5];
+        let should_be = SafetyLevel::Safe;
+
+        let report_safety = assess_level(&report, tolerance);
+
+        assert_eq!(should_be, report_safety);
+    }
+
+ #[test]
+    fn report_five_puzzle_two(){
+        let tolerance = 1;
+
+        let report = vec![8, 6, 4, 4, 1];
+        let should_be = SafetyLevel::Safe;
+
+        let report_safety = assess_level(&report, tolerance);
+
+        assert_eq!(should_be, report_safety);
+    }
+
+ #[test]
+    fn report_six_puzzle_two(){
+        let tolerance = 1;
+
+        let report = vec![1, 3, 6, 7, 9];
+        let should_be = SafetyLevel::Safe;
+
+        let report_safety = assess_level(&report, tolerance);
+
+        assert_eq!(should_be, report_safety);
+    }
+
+    #[test]
+    fn count_safe_reports_from_puzzle_two_example(){
+        let should_be = 4;
+
+        let reports = vec![
+                        vec![7, 6, 4, 2, 1],
+                        vec![1, 2, 7, 8, 9],
+                        vec![9, 7, 6, 2, 1],
+                        vec![1, 3, 2, 4, 5],
+                        vec![8, 6, 4, 4, 1],
+                        vec![1, 3, 6, 7, 9]
+                     ];
+
+        let num_safe_reports = count_safe_reports(reports, 1);
 
         assert_eq!(should_be, num_safe_reports)
     }
